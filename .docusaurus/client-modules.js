a97baf3a47bc3cfcf28920dda3e996ad1bb74224
Junhyun Bae <junhyun.bae.kr@gmail.com>
2025-04-16 16:45:53 +0900

    Initial commit with Docusaurus setup

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/junhyun/JunhyunB.github.io/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/junhyun/JunhyunB.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/junhyun/JunhyunB.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/junhyun/JunhyunB.github.io/src/css/custom.css"),
+];
